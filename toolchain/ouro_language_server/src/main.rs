e2891076cdef20f94d2c54eaafde242371250280
Quinn <qokabay1@swarthmore.edu>
2026-07-22 21:46:22 -0400

    delete comment

--- toolchain/ouro_language_server/src/main.rs
+++ toolchain/ouro_language_server/src/main.rs
@@ -80,54 +80,51 @@ async fn main() {
             uri_to_analysis: HashMap::new(),
         });
         router
-            .request::<lsp_request!("initialize"), _>(|_state, params| {
-                async move {
-                    eprintln!("Initialize with {params:?}");
-                    Ok(InitializeResult {
-                        capabilities: ServerCapabilities {
-                            position_encoding: None,
-                            // text_document_sync: None,
-                            text_document_sync: Some(TextDocumentSyncCapability::Kind(
-                                TextDocumentSyncKind::FULL,
-                            )),
-                            selection_range_provider: None,
-                            hover_provider: Some(HoverProviderCapability::Simple(true)),
-                            completion_provider: None,
-                            signature_help_provider: None,
-                            definition_provider: Some(OneOf::Left(true)),
-                            type_definition_provider: None,
-                            implementation_provider: None,
-                            references_provider: None,
-                            document_highlight_provider: None,
-                            document_symbol_provider: None,
-                            workspace_symbol_provider: None,
-                            code_action_provider: None,
-                            code_lens_provider: None,
-                            document_formatting_provider: None,
-                            document_range_formatting_provider: None,
-                            document_on_type_formatting_provider: None,
-                            rename_provider: None,
-                            document_link_provider: None,
-                            color_provider: None,
-                            folding_range_provider: None,
-                            declaration_provider: None,
-                            execute_command_provider: None,
-                            workspace: None,
-                            call_hierarchy_provider: None,
-                            semantic_tokens_provider: None,
-                            moniker_provider: None,
-                            linked_editing_range_provider: None,
-                            inline_value_provider: None,
-                            inlay_hint_provider: None,
-                            diagnostic_provider: None,
-                            experimental: None,
-                        },
-                        server_info: Some(ServerInfo {
-                            name: "Ouro Language Server".to_string(),
-                            version: None,
-                        }),
-                    })
-                }
+            .request::<lsp_request!("initialize"), _>(|_state, params| async move {
+                eprintln!("Initialize with {params:?}");
+                Ok(InitializeResult {
+                    capabilities: ServerCapabilities {
+                        position_encoding: None,
+                        text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                            TextDocumentSyncKind::FULL,
+                        )),
+                        selection_range_provider: None,
+                        hover_provider: Some(HoverProviderCapability::Simple(true)),
+                        completion_provider: None,
+                        signature_help_provider: None,
+                        definition_provider: Some(OneOf::Left(true)),
+                        type_definition_provider: None,
+                        implementation_provider: None,
+                        references_provider: None,
+                        document_highlight_provider: None,
+                        document_symbol_provider: None,
+                        workspace_symbol_provider: None,
+                        code_action_provider: None,
+                        code_lens_provider: None,
+                        document_formatting_provider: None,
+                        document_range_formatting_provider: None,
+                        document_on_type_formatting_provider: None,
+                        rename_provider: None,
+                        document_link_provider: None,
+                        color_provider: None,
+                        folding_range_provider: None,
+                        declaration_provider: None,
+                        execute_command_provider: None,
+                        workspace: None,
+                        call_hierarchy_provider: None,
+                        semantic_tokens_provider: None,
+                        moniker_provider: None,
+                        linked_editing_range_provider: None,
+                        inline_value_provider: None,
+                        inlay_hint_provider: None,
+                        diagnostic_provider: None,
+                        experimental: None,
+                    },
+                    server_info: Some(ServerInfo {
+                        name: "Ouro Language Server".to_string(),
+                        version: None,
+                    }),
+                })
             })
             .request::<lsp_request!("textDocument/hover"), _>(|state, params| {
                 let Some(analysis) = state
