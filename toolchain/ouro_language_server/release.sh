496647e8c639df6779c4b8634b9c987195e78a4d
Quinn <qokabay1@swarthmore.edu>
2026-07-22 22:04:01 -0400

    Delete release.sh

--- toolchain/ouro_language_server/release.sh
+++
@@ -1,5 +0,0 @@
-cargo build --bin ouro_language_server -r
-
-RELEASE_LOC="$(dirname "${BASH_SOURCE[0]}")/../../target/release/ouro_language_server"
-
-cp $RELEASE_LOC '/Users/quinn/Library/Application Support/Zed/languages/ouro_language_server'
