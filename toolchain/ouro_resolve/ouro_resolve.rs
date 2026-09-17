75aceb3dd21411344e473e18c451e3cae297288b
Quinn <qokabay1@swarthmore.edu>
2026-09-17 08:22:03 -0400

    monthly diff dump

--- toolchain/ouro_resolve/ouro_resolve.rs
+++ toolchain/ouro_resolve/ouro_resolve.rs
@@ -1,179 +1,119 @@
-use ouro_index_vec::{IndexSlice, IndexVec, MaxOr};
-use ouro_parse_node::{ExprKind, Node, NodeImpl, NodeKind, SynRef};
-use ouro_span::Byte;
-use ouro_tokenize::Token;
+use ouro_index_vec::{IndexVec, MaxOr};
+use ouro_parse_types::{ExprKind, Node, NodeKind, Nodes};
+use ouro_tokenize_types::ByteSpans;
 use std::collections::HashMap;
-use std::num::NonZero;
 
-struct Text<'a> {
-    ends: &'a IndexSlice<Token, [Byte]>,
-    source: &'a str,
+ouro_index_vec::define_index_type! {
+    pub struct Ref = u32;
+    DEBUG_FORMAT = "Ref({})";
 }
-
-impl<'a> Text<'a> {
-    fn as_str(&self, token: Token) -> &'a str {
-        ouro_tokenize::span(token, self.ends).lookup(self.source)
-    }
-}
-
-type ScopeId = NonZero<u32>;
-
-struct ScopeFrame {
-    scope_id: ScopeId,
-    truncate_defs_to: usize,
-}
-
-pub struct Resolver<'source> {
-    dedup: HashMap<&'source str, MaxOr<SynRef>>,
-    text: Text<'source>,
-
-    // TODO: next objective is to combine scope_stack and def_stack
-    // Importantly: need a way to have nodes that do not correspond to tokens.
-    // This goes up and down.
-    scope_stack: Vec<ScopeFrame>,
-    top_scope_id: ScopeId,
-
-    // The defs that are queued up to trigger at the end of their scope.
-    def_stack: Vec<Node>,
-
-    // where to get a fresh scope_id for scope_stack
-    next_scope_id: ScopeId,
-
-    // The final buffer that we return: iterate through this and you'll get where everything points
-    result: IndexVec<SynRef, Entry>,
-}
-
-// Instead of only storing ref to def, it would also be nice to store def to refs as well
-// in the buffer
 
 #[derive(Debug)]
-pub enum Entry {
-    Def(Node),
-    Undef {
-        scope_id: ScopeId,
-        next: MaxOr<SynRef>,
-    },
+pub struct RefImpl {
+    /// Either points to a Def or where the scope opened.
+    pub node: Node,
+
+    /// Linked list of other references to the same def.
+    pub next: MaxOr<Ref>,
 }
-const _: () = assert!(size_of::<Entry>() == 8);
+const _: () = assert!(size_of::<RefImpl>() == 8);
 
-// TODO(okabayashi): this isn't used yet, but we should make the result buffer store
-// refs AND defs and have them form cycles.
-pub struct Entry2 {
-    // for refs, these would point to either defs or their opening scope thingy
-    // for defs, this points to its opening scope thingy
-    node: Node,
-
-    // these all join point in a circularly linked list
-    next: SynRef,
+#[derive(Copy, Clone)]
+enum Inst {
+    Ref,
+    Def,
+    Open,
+    Close,
 }
-const _: () = assert!(size_of::<Entry2>() == 8);
 
-impl<'source> Resolver<'source> {
-    fn scope_open(&mut self) {
-        self.top_scope_id = self.next_scope_id;
-        self.scope_stack.push(ScopeFrame {
-            scope_id: self.next_scope_id,
-            truncate_defs_to: self.def_stack.len(),
-        });
-        self.next_scope_id = self.next_scope_id.saturating_add(1);
-    }
+impl Inst {
+    fn classify(node_kind: NodeKind) -> Option<Self> {
+        match node_kind {
+            // Def
+            NodeKind::FnIdent
+            | NodeKind::FnParamsIdent
+            | NodeKind::LetIdent
+            | NodeKind::ConstIdent => Some(Inst::Def),
 
-    fn scope_close(&mut self, nodes: &IndexSlice<Node, [NodeImpl]>) {
-        let scope_frame = self
-            .scope_stack
-            .pop()
-            .expect("matched by a preceeding push");
-        self.top_scope_id = scope_frame.scope_id;
+            // Ref
+            NodeKind::Expr(ExprKind::Ident) => Some(Inst::Ref),
 
-        for def in self.def_stack.drain(scope_frame.truncate_defs_to..).rev() {
-            let string = self.text.as_str(nodes[def].token);
-            let Some(slot) = self.dedup.get_mut(string) else {
-                continue;
-            };
+            // Open
+            NodeKind::FileBegin
+            | NodeKind::StructBodyBegin
+            | NodeKind::FnParams
+            | NodeKind::Expr(ExprKind::Block) => Some(Inst::Open),
 
-            let mut max_or_entry = *slot;
+            // Close
+            NodeKind::FileEnd
+            | NodeKind::StructBodyEnd
+            | NodeKind::FnBodyEnd
+            | NodeKind::Expr(ExprKind::BlockEnd) => Some(Inst::Close),
 
-            while let Some(synref) = max_or_entry.into_non_max() {
-                let Entry::Undef {
-                    scope_id: result_scope_id,
-                    next,
-                } = self.result[synref]
-                else {
-                    unreachable!();
-                };
-                if result_scope_id < self.top_scope_id {
-                    break;
-                }
-                // We're on an entry that points to a slot that needs to be rewritten!
-                self.result[synref] = Entry::Def(def);
-
-                max_or_entry = next;
-            }
-            // Put what we finished on back
-            *slot = max_or_entry;
+            // Everything else
+            _ => None,
         }
     }
+}
+
+pub fn resolve(nodes: &Nodes, spans: &ByteSpans, source: &str) -> Resolve {
+    let mut top_start = Node::from_usize(0);
+    let mut stack = Vec::new();
+    let mut dedup = HashMap::new();
+    let mut refs = IndexVec::new();
+
+    for (node, node_kind) in nodes.nodes.iter_enumerated() {
+        match Inst::classify(*node_kind) {
+            None => {}
+            Some(Inst::Def) => stack.push(node),
+            Some(Inst::Ref) => {
+                let head = dedup
+                    .entry(spans.to_span(nodes.tokens[node]).lookup(source))
+                    .or_insert(MaxOr::max());
+                *head = MaxOr::new(refs.push(RefImpl {
+                    node: top_start,
+                    next: *head,
+                }));
+            }
+            Some(Inst::Open) => {
+                stack.push(top_start);
+                top_start = node;
+            }
+            Some(Inst::Close) => {
+                loop {
+                    let popped_node = stack.pop().expect("shouldn't run out of elements because we stop as soon as we hit matching open");
+                    match Inst::classify(nodes.nodes[popped_node]) {
+                        Some(Inst::Open) => {
+                            top_start = popped_node;
+                            break;
+                        }
+                        Some(Inst::Def) => {
+                            let source = spans.to_span(nodes.tokens[popped_node]).lookup(source);
+                            let Some(head) = dedup.get_mut(source) else {
+                                continue;
+                            };
+
+                            while let Some(head_ref) = head.into_non_max() {
+                                if refs[head_ref].node < top_start {
+                                    break;
+                                }
+                                refs[head_ref].node = popped_node;
+                                *head = refs[head_ref].next;
+                            }
+                        }
+                        _ => unreachable!(
+                            "should never reach here because we only push nodes that are classified as Def or Open"
+                        ),
+                    }
+                }
+            }
+        }
+    }
+
+    Resolve { refs }
 }
 
 #[derive(Debug)]
 pub struct Resolve {
-    pub ref_to_referent: IndexVec<SynRef, Entry>,
-}
-
-// 1{ def ref } 2{ ref def }
-// go forward until you find a def, ensure def is visible from first ref, then keep going until that def goes out of scope
-// { def { def } } -> "I won't look at anything before my ScopeId"
-//
-// { ref } { def } -> "I wont look at anything before my ScopeId"
-
-pub fn resolve(
-    nodes: &IndexSlice<Node, [NodeImpl]>,
-    ends: &IndexSlice<Token, [Byte]>,
-    source: &str,
-) -> Resolve {
-    let mut resolver = Resolver {
-        dedup: HashMap::new(),
-        text: Text { ends, source },
-        scope_stack: Vec::new(),
-        top_scope_id: const { NonZero::new(1).unwrap() },
-        def_stack: Vec::new(),
-        next_scope_id: const { NonZero::new(1).unwrap() },
-        result: IndexVec::new(),
-    };
-
-    resolver.scope_open();
-    for (node, node_impl) in nodes.iter_enumerated() {
-        match node_impl.kind {
-            NodeKind::FnIdent
-            | NodeKind::FnParamsIdent
-            | NodeKind::LetIdent
-            | NodeKind::ConstIdent => {
-                resolver.def_stack.push(node);
-            }
-            NodeKind::Expr(ExprKind::Ident) => {
-                let slot = resolver
-                    .dedup
-                    .entry(resolver.text.as_str(node_impl.token))
-                    .or_insert(MaxOr::max());
-                let next = *slot;
-                *slot = MaxOr::new(resolver.result.push(Entry::Undef {
-                    scope_id: resolver.top_scope_id,
-                    next,
-                }));
-            }
-            NodeKind::StructBodyBegin | NodeKind::FnParams | NodeKind::Expr(ExprKind::Block) => {
-                resolver.scope_open();
-                // resolver.def_stack.push(node);
-            }
-            NodeKind::StructBodyEnd | NodeKind::FnBodyEnd | NodeKind::Expr(ExprKind::BlockEnd) => {
-                resolver.scope_close(nodes);
-            }
-            _ => {}
-        }
-    }
-    resolver.scope_close(nodes);
-
-    Resolve {
-        ref_to_referent: resolver.result,
-    }
+    pub refs: IndexVec<Ref, RefImpl>,
 }
--- toolchain/ouro_resolve/ouro_resolve_test.rs
+++ toolchain/ouro_resolve/ouro_resolve_test.rs
@@ -1,6 +1,5 @@
 use ouro_parse::parse;
-use ouro_parse_node::{ExprKind, NodeKind};
-use ouro_pprint::pprint;
+use ouro_parse_types::{ExprKind, NodeKind};
 use ouro_resolve::*;
 use ouro_tokenize::tokenize;
 
@@ -10,19 +9,26 @@ fn pprint_name_resolve(input: &str) -> String {
     let tokenize = tokenize(input);
     let parse = parse(&tokenize.tokens);
     parse.ok.as_ref().unwrap();
-    let resolve = resolve(&parse.nodes, &tokenize.ends, input);
-    let mut iter = resolve.ref_to_referent.into_iter();
+    let resolve = resolve(&parse.nodes, &tokenize.spans, input);
+    let mut iter = resolve.refs.into_iter();
 
-    let out = pprint(&parse, |node, out| {
-        let node_impl = &parse.nodes[node];
-        let string = ouro_tokenize::span(node_impl.token, &tokenize.ends).lookup(input);
-        write!(out, "{:?} {:?} {:?}", node, node_impl.kind, string).unwrap();
+    let mut out = String::new();
+    for (node, &node_kind) in parse.nodes.nodes.iter_enumerated() {
+        write!(out, "{node:?} {node_kind:?}").unwrap();
+        if node_kind.has_token() {
+            let string = tokenize
+                .spans
+                .to_span(parse.nodes.tokens[node])
+                .lookup(input);
+            write!(out, " {string:?}").unwrap();
+        }
 
-        if let NodeKind::Expr(ExprKind::Ident) = node_impl.kind {
+        if let NodeKind::Expr(ExprKind::Ident) = node_kind {
             let opt_def = iter.next().expect("should align");
             write!(out, " -> {opt_def:?}").unwrap();
         }
-    });
+        writeln!(out).unwrap();
+    }
     assert!(iter.next().is_none(), "didn't consume all synrefs?");
     out
 }
@@ -76,6 +82,53 @@ fn test_hello() {
 
         fn main() {
             // stuff
+        }
+    });
+}
+
+#[test]
+fn debug() {
+    insta::assert_snapshot!(case! {
+        const Thing = struct {
+            const Self = @This();
+
+            fn new(self: Self) {
+                self + i32
+            }
+        };
+
+        fn VecImpl(T: type) {
+            // When we enter a struct body, everything in here is runtime dependent.
+            // Even types in type signatures.
+            struct {
+                // Runtime dep on this
+                t: T,
+                y: foo(),
+            }
+        }
+
+        fn Vec(T: type) {
+            VecImpl(T)
+        }
+
+        fn foo() {}
+
+        // Since Vec depends on VecImpl, we need to ensure that we're below that.
+        // Just ensure that the decl we're in is below the last decl that Vec uses
+        fn print_ints(ints: Vec(i32)) {
+            // stuff
+        }
+
+        fn print(s: string) {
+            struct {
+                fn fmt(io: Io) {
+                    // print the stuff
+                }
+            }.fmt
+        }
+
+        pub fn main(io: Io) {
+            print("hello")(io)
         }
     });
 }
